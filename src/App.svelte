4b01e928547ff4064e23be0f6b53eaeca5156de8
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-07-14 13:50:50 +0200

    Add some styles.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,18 @@
 <div id="layout">
   <nav>Navbar</nav>
-  <aside>Side Nav</aside>
+  <aside>
+    <ul id="menu">
+      <li><a href="#">Scherbenviertel</a></li>
+      <li><a href="#">Lesungen</a></li>
+      <li><a href="#">Gedanken und Geschichten</a></li>
+      <li><a href="#">Vita</a></li>
+      <li><a href="#">Kontakt</a></li>
+    </ul>
+  </aside>
   <main>
     <h1>Svelte Playground</h1>
   </main>
-  <footer>Footer</footer>
+  <footer>Scherbenviertel - Ines Siegfried</footer>
 </div>
 
 <style>
@@ -17,9 +25,12 @@
     --purple-light: #A91060;
     --white: #EBEBEB;
     --black: #000000;
+    --font-size: 16pt;
   }
   
   #layout {
+    color: var(--white);
+    font-size: var(--font-size);
     background: radial-gradient(circle at center, var(--purple-dark), var(--purple-darker));
     display: grid;
     grid-template-columns: 1fr 300px;
@@ -30,6 +41,7 @@
     "footer footer";
     min-height: 100vh;
   }
+
   main {
     grid-area: main;
     overflow-y: scroll;
@@ -50,7 +62,12 @@
   footer {
     height: var(--footer-height);
     grid-area: footer;
-    background-color: magenta;
+    background-color: var(--purple);
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
   }
 
   h1 {
@@ -59,4 +76,20 @@
     font-size: 4em;
     font-weight: 100;
   }
+
+  #menu {
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: center;
+    height: 100%;
+    gap: 0.5rem;
+  }
+
+  #menu > li > a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+
 </style>
