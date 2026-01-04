e37caf70f924c6b25f7c384174d63d398af1f8a8
Cedric Geissmann <cedric.geissmann@gmail.com>
2026-01-04 09:56:15 +0100

    Remove generated files from git.

--- .routify/render.js
+++
@@ -1,6 +0,0 @@
-
-        import * as module from '../src/App.svelte'
-        import { renderModule } from '@roxi/routify/tools/ssr5.js'
-        import { map } from './route-map.js'
-
-        export const render = url => renderModule(module, { url, routesMap: map })
